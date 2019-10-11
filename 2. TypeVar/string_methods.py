95bcc7129b6331c7d30f433585bf1cc5526019fe
bigazzzz <bigazzzz@gmail.com>
2019-10-11 13:58:03 +0300

    Add string method

---
+++ 2. TypeVar/string_methods.py
@@ -0,0 +1,9 @@
+text = "HELLo, wORld!"
+print(text.replace("w","W")) # HELLo, WORld!
+print(text.lower()) # hello, world!
+print(text.capitalize()) # Hello, world!
+print(text.title()) # Hello, World!
+print(text.lower().replace('world', 'pentester').upper())
+# HELLO, PENTESTER!
+
+
--- 2. TypeVar/strings.py
+++ 2. TypeVar/strings.py
@@ -1,13 +1,23 @@
+print("-"*10, "\n1 часть", "\n", "-"*10)
 text="Hello, world"
 print(len(text)) # количество симоволов 12
 print(text[0]) # первый символ H
 print(text[-1]) # последний символ d
 print(text[3-5]) # что будет?
 
+print("-"*10, "\n2 часть", "\n", "-"*10)
+text="Hello, world"
+print(text[1:5]) # ello
+print(text[0:5]) # Hello
+print(text[:5]) # Hello
+print(text[-5:]) # world
+
+print("-"*10, "\n3 часть", "\n", "-"*10)
 CARDNUMBER = 4242_1543_1456_0102
 print(CARDNUMBER + 1) # 4242154314560103
 print(str(CARDNUMBER) + "1") # 42421543145601021
 
+print("-"*10, "\n4 часть", "\n", "-"*10)
 i = "i love "
 app1 = "w3af"
 app2 = "nmap"
@@ -19,6 +29,8 @@ print(i + app1,app2, app3, sep=", ") # i love w3af, nmap, metasploit
 print(i + app1,app2, app3, sep=", ", end=".") # i love w3af, nmap, metasploit.
 print(i + app1,app2, app3, sep=", ", end=".\n") # i love w3af, nmap, metasploit.
 print(i, 'pentesting') # i love  pentesting
+print((i+'pentesting'+' ')*5)
+# i love pentesting i love pentesting i love pentesting i love pentesting i love pentesting
 
 
 
